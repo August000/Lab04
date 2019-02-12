13163f37b73e119dc13befd611f44ecc466802f0
Augusto Batista <augusto.batista@ou.edu>
2019-02-11 22:39:25 -0600

    Created a test for Zoo class

--- src/Todo.java
+++ src/Todo.java
@@ -7,12 +7,19 @@ OU EMAIL____________: augusto.batista@ou.edu
 Tasks:
 1. complete Equipment equals method
 2. implement testFullConstructorMethod
+3. implement equipment equals method
+4. create EquipmentTest class
 
 Estimated Time (minutes):
 1. 15
 2. 6
+3. 5
+4. 40
 
 Actual Time (minutes):
 1. 24
 2. 10
+3. 9
+4.
+
 */}
---
+++ src/ZooTest.java
@@ -0,0 +1,78 @@
+ 
+public class ZooTest {
+	
+	
+	public void fullConstructorTest() throws AssertException{
+	
+		Zoo zoo = new Zoo(4);
+		
+		Assert.assertEquals(4, zoo.getAnimals().length);
+		Assert.assertEquals(4, zoo.getCapacity());
+	}
+	
+	public void expandZooTest() throws AssertException{
+		
+		Zoo zoo = new Zoo(1);
+		Animal ani1 = new Animal("Weasel","White",10,5);
+		Animal ani2 = new Animal("Tiger","White",200,50);
+		zoo.addAnimal(ani1);
+		zoo.addAnimal(ani2);
+		
+		Assert.assertEquals(2, zoo.getCapacity());
+	}
+	
+	public void addAnimalTest() throws AssertException{
+		
+		Zoo zoo = new Zoo(2);
+		
+		Animal ani = new Animal("Weasel","White",10,5); 
+		zoo.addAnimal(ani);
+		
+		Assert.assertEquals(ani, zoo.getAnimals()[0]);
+		
+	}
+	
+	public void totalHeightTest() throws AssertException{
+		
+		Zoo zoo = new Zoo(2);
+		Animal ani1 = new Animal("Weasel","White",10,5);
+		Animal ani2 = new Animal("Tiger","White",200,50);
+		zoo.addAnimal(ani1);
+		zoo.addAnimal(ani2);
+		
+		Assert.assertEquals(55.0, zoo.getTotalHeight());
+	}
+	
+	public void getAverageWeightDefautTest() throws AssertException {
+		
+		Zoo zoo = new Zoo(3);
+		Animal ani1 = new Animal("Weasel","White",20,5);
+		Animal ani2 = new Animal("Tiger","White",200,50);
+		zoo.addAnimal(ani1);
+		zoo.addAnimal(ani2);
+		
+		Assert.assertEquals(110, zoo.getAverageWeight());
+	}
+	
+	public void getAverageWeightColorTest() throws AssertException {
+		
+		Zoo zoo = new Zoo(4);
+		Animal ani1 = new Animal("Weasel","White",20,5);
+		Animal ani2 = new Animal("Tiger","White",200,50);
+		Animal ani3 = new Animal("Whale","Black",1000,200);
+		zoo.addAnimal(ani1);
+		zoo.addAnimal(ani2);
+		zoo.addAnimal(ani3);
+		
+		Assert.assertEquals(110, zoo.getAverageWeight("white"));
+	}
+	
+
+	
+	
+	
+	
+	
+	
+}
+
